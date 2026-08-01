383256aa0ce3fd90ce6d673481a4ac3c20b1adf8
Franciele Torres <mariafrancielesm@gmail.com>
2026-08-01 01:21:46 +0100

    Create Invoice Model

---
+++ WaterManagementSystem/WaterManagementSystem.Wpf/Models/Invoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterManagementSystem.Wpf.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+
+        public int ConsumptionId { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public decimal InvoiceAmount { get; set; }
+
+        public bool IsCancelled { get; set; }
+
+        public bool IsPaid { get; set; }
+
+        public int MeterId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerAddress { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public string TaxNumber { get; set; }
+
+        public DateTime ReadingDate { get; set; }
+
+        public int MeterReading { get; set; }
+
+        public decimal ConsumedVolume { get; set; }
+
+    }
+}
