774c6746c8c2e523aa81af1b355090d943da6844
Franciele Torres <mariafrancielesm@gmail.com>
2026-08-02 01:36:01 +0100

    Add Invoice logic and Invoice Details window

---
+++ WaterManagementSystem/WaterManagementSystem.Wpf/Windows/InvoiceDetailsWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WaterManagementSystem.Wpf.Models;
+
+namespace WaterManagementSystem.Wpf.Windows
+{
+    /// <summary>
+    /// Interaction logic for InvoiceDetailsWindow.xaml
+    /// </summary>
+    public partial class InvoiceDetailsWindow : Window
+    {
+        private Invoice _invoice;
+        public InvoiceDetailsWindow(Invoice invoice)
+        {
+            InitializeComponent();
+            _invoice = invoice;
+
+
+            LoadInvoiceDetails();
+            
+        }
+
+        private void LoadInvoiceDetails()
+        {
+            txtCustomerName.Text = _invoice.CustomerName;
+            txtTaxNumber.Text = _invoice.TaxNumber;
+            txtCustomerAddress.Text = _invoice.CustomerAddress;
+            txtCustomerPhone.Text = _invoice.CustomerPhone;
+            txtCustomerEmail.Text = _invoice.CustomerEmail;
+
+            txtMeterId.Text = _invoice.MeterId.ToString();
+            txtReadingDate.Text = _invoice.ReadingDate.ToString("dd/MM/yyyy");
+            txtMeterReading.Text = _invoice.MeterReading.ToString();
+            txtConsumedVolume.Text = _invoice.ConsumedVolume.ToString("F2") + " m³";
+
+            txtInvoiceId.Text = _invoice.InvoiceId.ToString();
+            txtIssueDate.Text = _invoice.IssueDate.ToString("dd/MM/yyyy");
+            txtInvoiceAmount.Text = _invoice.InvoiceAmount.ToString("F2");
+
+            if (_invoice.IsCancelled)
+            {
+                txtInvoiceStatus.Text = "Cancelada";
+            }
+            else if (_invoice.IsPaid)
+            {
+                txtInvoiceStatus.Text = "Paga";
+            }
+            else
+            {
+                txtInvoiceStatus.Text = "Não paga";
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- WaterManagementSystem/WaterManagementSystem.Wpf/Windows/InvoiceWindow.xaml.cs
+++ WaterManagementSystem/WaterManagementSystem.Wpf/Windows/InvoiceWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Policy;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WaterManagementSystem.Wpf.Models;
+using WaterManagementSystem.Wpf.Services;
 
 namespace WaterManagementSystem.Wpf.Windows
 {
@@ -17,9 +22,430 @@ namespace WaterManagementSystem.Wpf.Windows
     /// </summary>
     public partial class InvoiceWindow : Window
     {
+        private readonly ApiService _apiService;
+
         public InvoiceWindow()
         {
             InitializeComponent();
+            _apiService = new ApiService();
+
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadCustomers();
+            await LoadInvoices();
+        }
+
+        private async Task LoadInvoices()
+        {
+            Response response = await _apiService.GetInvoices();
+
+            if (response.IsSuccess)
+            {
+                var invoices = (List<Invoice>)response.Result;
+
+                dgInvoices.ItemsSource = invoices;
+            }
+            else
+            {
+                MessageBox.Show(response.Message, "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private async Task LoadCustomers()
+        {
+            Response response = await _apiService.GetCustomers();
+
+            if (response.IsSuccess)
+            {
+                var customers = (List<Customer>)response.Result;
+
+                cbCustomers.ItemsSource = customers;
+                cbFilterCustomer.ItemsSource = customers;
+            }
+            else
+            {
+                MessageBox.Show(response.Message, "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private async void cbCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbCustomers.SelectedItem == null)
+            {
+                return;
+            }
+
+            cbMeters.ItemsSource = null;
+            cbConsumptions.ItemsSource = null;
+
+            Customer selectedCustomer =
+                (Customer)cbCustomers.SelectedItem;
+
+            await LoadMeters(selectedCustomer.CustomerId);
+        }
+
+        private async Task LoadMeters(int customerId)
+        {
+            Response response =
+            await _apiService.GetMetersByCustomer(customerId);
+
+            if (response.IsSuccess)
+            {
+                List<Meter> meters = (List<Meter>)response.Result;
+
+                cbMeters.ItemsSource = meters;
+            }
+            else
+            {
+                cbMeters.ItemsSource = null;
+
+                MessageBox.Show(response.Message, "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+        }
+
+        private async void cbMeters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbMeters.SelectedItem == null)
+            {
+                return;
+            }
+
+            cbConsumptions.ItemsSource = null;
+
+            Meter selectedMeter = (Meter)cbMeters.SelectedItem;
+
+            await LoadConsumptions(selectedMeter.MeterId);
+        }
+
+        private async Task LoadConsumptions(int meterId)
+        {
+            Response response = await _apiService.GetConsumptionsByMeter(meterId);
+
+            if (response.IsSuccess)
+            {
+                List<Consumption> consumptions = (List<Consumption>)response.Result;
+
+                cbConsumptions.ItemsSource = consumptions;
+            }
+            else
+            {
+                cbConsumptions.ItemsSource = null;
+
+                MessageBox.Show(response.Message, "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+        }
+
+        private void ClearFields()
+        {
+            cbCustomers.SelectedItem = null;
+            cbMeters.ItemsSource = null;
+            cbConsumptions.ItemsSource = null;
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFields();
+        }
+
+        private async void btnCreateInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (cbConsumptions.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione um consumo.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                return;
+            }
+
+            Consumption selectedConsumption = (Consumption)cbConsumptions.SelectedItem;
+
+            Invoice newInvoice;
+
+            newInvoice = new Invoice
+            {
+                ConsumptionId = selectedConsumption.ConsumptionId
+            };
+
+            Response response = await _apiService.CreateInvoice(newInvoice);
+
+            if (response.IsSuccess)
+            {
+                MessageBox.Show(response.Message,
+                    "Sucesso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                await LoadInvoices();
+
+                ClearFields();
+            }
+            else
+            {
+                MessageBox.Show(response.Message,
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private async Task ClearFilterFields()
+        {
+            cbFilterCustomer.SelectedItem = null;
+            dpStartDate.SelectedDate = null;
+            dpEndDate.SelectedDate = null;
+            cbPaymentStatus.SelectedIndex = 0;
+
+            await LoadInvoices();
+        }
+        private async void btnClearFilters_Click(object sender, RoutedEventArgs e)
+        {
+            await ClearFilterFields();
+        }
+
+        private async void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            int? customerId = null;
+            DateTime? startDate = dpStartDate.SelectedDate;
+            DateTime? endDate = dpEndDate.SelectedDate;
+            bool? isPaid = null;
+            bool? isCancelled = null;
+
+            if (cbFilterCustomer.SelectedItem != null)
+            {
+                Customer selectedCustomer = (Customer)cbFilterCustomer.SelectedItem;
+
+                customerId = selectedCustomer.CustomerId;
+            }
+
+
+            if (startDate.HasValue && endDate.HasValue && startDate.Value.Date > endDate.Value.Date)
+            {
+                MessageBox.Show("A data inicial não pode ser posterior à data final.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            if (cbPaymentStatus.SelectedIndex == 1)
+            {
+                isPaid = true;
+                isCancelled = false;
+            }
+            else if (cbPaymentStatus.SelectedIndex == 2)
+            {
+                isPaid = false;
+                isCancelled = false;
+            }
+            else if (cbPaymentStatus.SelectedIndex == 3)
+            {
+                isCancelled = true;
+            }
+
+            Response response = await _apiService.SearchInvoices(
+                    customerId,
+                    startDate,
+                    endDate,
+                    isPaid,
+                    isCancelled);
+
+            if (response.IsSuccess)
+            {
+                List<Invoice> invoices = (List<Invoice>)response.Result;
+
+                dgInvoices.ItemsSource = invoices;
+            }
+            else
+            {
+                dgInvoices.ItemsSource = null;
+
+                MessageBox.Show(response.Message,
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+        }
+
+
+        private async void btnMarkAsPaid_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgInvoices.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione uma fatura.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            Invoice selectedInvoice = (Invoice)dgInvoices.SelectedItem;
+
+            if (selectedInvoice.IsCancelled)
+            {
+                MessageBox.Show("Não é possível pagar uma fatura cancelada.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            if (selectedInvoice.IsPaid)
+            {
+                MessageBox.Show("Esta fatura já está paga.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show("Deseja marcar esta fatura como paga?",
+                "Confirmação",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            selectedInvoice.IsPaid = true;
+            selectedInvoice.IsCancelled = false;
+
+            Response response = await _apiService.UpdateInvoice(selectedInvoice);
+
+            if (response.IsSuccess)
+            {
+                MessageBox.Show("Fatura marcada como paga.",
+                    "Sucesso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                await LoadInvoices();
+            }
+            else
+            {
+                MessageBox.Show(response.Message,
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private async void btnCancelInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgInvoices.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione uma fatura.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            Invoice selectedInvoice = (Invoice)dgInvoices.SelectedItem;
+
+            if (selectedInvoice.IsCancelled)
+            {
+                MessageBox.Show("Esta fatura já está cancelada.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                return;
+            }
+
+            if (selectedInvoice.IsPaid)
+            {
+                MessageBox.Show("Não é possível cancelar uma fatura paga.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show("Deseja realmente cancelar esta fatura?",
+                "Confirmação",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            selectedInvoice.IsCancelled = true;
+            selectedInvoice.IsPaid = false;
+
+            Response response = await _apiService.UpdateInvoice(selectedInvoice);
+
+            if (response.IsSuccess)
+            {
+                MessageBox.Show("Fatura cancelada com sucesso.",
+                    "Sucesso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                await LoadInvoices();
+            }
+            else
+            {
+                MessageBox.Show(response.Message,
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private async void btnViewInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgInvoices.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione uma fatura.",
+                    "Aviso",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            Invoice selectedInvoice = (Invoice)dgInvoices.SelectedItem;
+
+            Response response = await _apiService.GetInvoiceById(selectedInvoice.InvoiceId);
+
+            if (response.IsSuccess)
+            {
+                Invoice invoice =
+                    (Invoice)response.Result;
+
+                InvoiceDetailsWindow invoiceDetailsWindow = new InvoiceDetailsWindow(invoice);
+                invoiceDetailsWindow.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show(response.Message,
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+
         }
     }
 }
